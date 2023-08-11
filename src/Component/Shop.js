eb78265ecb63102c73f3c702be74b155d4a89af8
Abhinay Kumar <kabhinayak02@gmail.com>
2023-08-11 21:46:04 +0530

    implemented  bindActionCreators

--- src/Component/Shop.js
+++ src/Component/Shop.js
@@ -1,16 +1,19 @@
 import React from 'react'
-import { useDispatch } from 'react-redux'
-// import { bindActionCreators } from 'redux';
+import { useDispatch, useSelector } from 'react-redux'
+import { bindActionCreators } from 'redux';
 import {actionCreators} from '../State/index';
 
 const Shop = () => {
+
   const dispatch = useDispatch();
+  const {withdrawMoney, depositMoney} = bindActionCreators(actionCreators, dispatch);
+  const amount = useSelector(state => state.amount);
   return (
     <div>
         <h2>Deposit/Withdraw Money</h2>
-        <div className="btn btn-primary mx-2 mt-2" onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}}>-</div>
-            Update Balance
-        <div className="btn btn-primary mx-2 mt-2" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</div>
+        <div className="btn btn-primary mx-2 mt-2" onClick={()=>{withdrawMoney(100)}}>-</div>
+            Update Balance: {amount}
+        <div className="btn btn-primary mx-2 mt-2" onClick={()=>{depositMoney(100)}}>+</div>
     </div>
   )
 }
